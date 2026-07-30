316d7342a6ea0051645c54015e2b96bbc976326f
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-07-30 15:19:15 +0200

    Update bot_micropulseweex.js

--- bot_micropulseweex.js
+++ bot_micropulseweex.js
@@ -271,7 +271,7 @@ async function mainLoop() {
   for (const symbol of ACTIVE_CRYPTO_LIST) {
     for (const timeframe of TIMEFRAMES) {
       try {
-        await processSymbol(symbol, timeframe);
+        //await processSymbol(symbol, timeframe);
       } catch (err) {
         console.log("Error processant", symbol, timeframe, err.message);
       }
@@ -285,7 +285,7 @@ async function mainLoop() {
 async function startBot() {
   await initDB();
   console.log("Bot MICRO‑PULSE en marxa (patrons + ATR + tracking + ordres)");
-  cron.schedule("* * * * *", mainLoop);
+  cron.schedule("0 * * * *", mainLoop);
 }
 
 startBot();
